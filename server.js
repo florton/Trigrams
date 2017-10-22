005230e5c36953c71f3ee87615ec6ce822508a97
Flanders Lorton <florton@lion.lmu.edu>
2017-10-22 14:59:30 -0700

    yeah

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let trigrams = () => {
       console.log(req.query);
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      let reading = iChing.ask(req.query.question);
+      let reading = iChing.ask(req.query.question || "");
       res.send(reading);
   });
 }
