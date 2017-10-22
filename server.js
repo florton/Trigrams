a57d6792f9237dbf052ca83b0b3cbe940bb6895b
Flanders Lorton <florton@lion.lmu.edu>
2017-10-22 14:55:01 -0700

    NodeJS I-Ching API

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let iChing = require('i-ching');
+
+let trigrams = () => {
+  app.get('/', (req, res) => {
+      console.log(req.query);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      let reading = iChing.ask(req.query.question);
+      res.send(reading);
+  });
+}
+
+trigrams();
+app.listen(process.env.PORT || 3000);
